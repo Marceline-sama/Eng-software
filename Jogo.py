04627b8086831de8916ed3785c45eb224831e593
Claybson Jordão <oninoniisan@gmail.com>
2024-09-13 15:12:03 -0300

    update jogo.py

--- Jogo.py
+++ Jogo.py
@@ -1,18 +1,21 @@
+import threading
+
 class Jogo:
     _instance = None
+    _lock = threading.Lock()  # Adicionando o lock
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(Jogo, cls).__new__(cls)
-            # Inicializando o estado do jogo
-            cls._instance.jogadores = []
-            cls._instance.cores_disponiveis = ["vermelho", "azul", "verde", "amarelo", "preto"]
-            cls._instance.territorios = ["territorio1", "territorio2", "territorio3", "territorio4", "territorio5"]
-            cls._instance.baralho_de_cartas = ["carta1", "carta2", "carta3", "carta4"]
-            cls._instance.objetivos = [
-                "Conquistar 24 territórios", 
-                "Eliminar o jogador vermelho", 
-                "Conquistar 18 territórios com 2 exércitos em cada"
-            ]
-            cls._instance.dados_jogadores = {}
-        return cls._instance
+    def __init__(self):
+        if Jogo._instance is not None:
+            raise Exception("Esta classe é um singleton!")
+        self.jogadores = {}
+        self.cores_disponiveis = ["vermelho", "azul", "verde", "amarelo", "preto"]
+        self.territorios = ["territorio1", "territorio2", "territorio3", "territorio4", "territorio5"]
+        self.baralho_de_cartas = ["carta1", "carta2", "carta3", "carta4"]
+        self.objetivos = ["Conquistar 24 territórios", "Eliminar o jogador vermelho", "Conquistar 18 territórios com 2 exércitos em cada"]
+
+    @staticmethod
+    def get_instance():
+        with Jogo._lock:
+            if Jogo._instance is None:
+                Jogo._instance = Jogo()
+            return Jogo._instance
